589cb7decda7837b2d04b3f63da0e7fb451a5a31
Adam Barcewicz <barcel91@gmail.com>
2026-05-16 11:35:46 +0200

    validationMessage added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useState} from "react";
+
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('barcel@buziaczek.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
+
+    let validationMessage = '';
+
+       if (email.length < 5) {
+            validationMessage = 'Ale masz małego maila';
+        } else if (email.length < 15) {
+            validationMessage = 'Masz spoko maila';
+        } else {
+            validationMessage = 'Masz za długiego maila';
+        }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <input type="text" value={email} onChange={handleChange}/>
+            {email.length > 0  && <div> Błąd: {validationMessage}</div>}
         </div>
     );
 }
